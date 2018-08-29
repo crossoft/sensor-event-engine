be3372978a88dbe53828d61a403316ea406186b9
Domas Bitvinskas <domas.bitvinskas@me.com>
2018-08-29 17:45:11 +0000

    Add percentChange not be called test

--- tests/rules/percentChange.test.js
+++ tests/rules/percentChange.test.js
@@ -11,7 +11,7 @@ beforeEach(async () => {
   await db.sync({ force: true })
 })
 
-test('basic should be called', async () => {
+test('should be called', async () => {
   const rule = {
     condition: {
       value: {
@@ -54,4 +54,49 @@ test('basic should be called', async () => {
   await apply([rule], event, cb)
 
   expect(cb).toBeCalledWith(rule)
+})
+
+test('should not be called', async () => {
+  const rule = {
+    condition: {
+      value: {
+        name: 'temperature',
+        aggregate: {
+          type: 'percentChange',
+          period: {
+            value: 5,
+            unit: 'days'
+          }
+        }
+      },
+      comparison: 'gt',
+      threshold: 100
+    },
+  }
+
+  await Event.create({
+    type: 'temperature',
+    dateTime: moment().subtract(4, 'days').format(),
+    readings: [{
+      name: 'temperature',
+      value: 10,
+    }],
+  }, {
+    include: [Sensor, Reading],
+  })
+
+  const event = await Event.create({
+    type: 'temperature',
+    readings: [{
+      name: 'temperature',
+      value: 15,
+    }],
+  }, {
+    include: [Sensor, Reading],
+  })
+
+  const cb = jest.fn()
+  await apply([rule], event, cb)
+
+  expect(cb).not.toBeCalled()
 })
